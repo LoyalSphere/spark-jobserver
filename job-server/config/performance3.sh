bf35bae8e0397a4438744d3142bff2ac10f330ec
Edward Samson <edward.samson@stellarloyalty.com>
2018-09-06 15:16:24 +0800

    Set SPARK_VERSION to 2.3.0

--- job-server/config/performance3.sh
+++ job-server/config/performance3.sh
@@ -10,16 +10,16 @@ APP_GROUP=spark
 JMX_PORT=9999
 # optional SSH Key to login to deploy server
 #SSH_KEY=/path/to/keyfile.pem
-INSTALL_DIR=/home/spark/job-server
-LOG_DIR=/var/log/job-server
+INSTALL_DIR=/opt/sjs
+LOG_DIR=/var/log/spark-job-server
 PIDFILE=spark-jobserver.pid
 JOBSERVER_MEMORY=1G
-SPARK_VERSION=1.6.0
-MAX_DIRECT_MEMORY=512M
-SPARK_HOME=/home/spark/spark-1.6.0
+SPARK_VERSION=2.3.0
+MAX_DIRECT_MEMORY=1G
+SPARK_HOME=/usr/hdp/current/spark2-client/
 SPARK_CONF_DIR=$SPARK_HOME/conf
 # Only needed for Mesos deploys
-SPARK_EXECUTOR_URI=/home/spark/spark-1.6.0.tar.gz
+# SPARK_EXECUTOR_URI=/home/spark/spark-1.6.0.tar.gz
 # Only needed for YARN running outside of the cluster
 # You will need to COPY these files from your cluster to the remote machine
 # Normally these are kept on the cluster in /etc/hadoop/conf
